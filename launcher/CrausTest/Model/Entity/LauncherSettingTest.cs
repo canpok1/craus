3f2469a4ccf316d6aa0c67211a0525cf826cd2c6
Tanabe <6isstrong@gmail.com>
2013-04-29 15:55:14 +0900

    LauncherSettingを実装

--- launcher/CrausTest/Model/Entity/LauncherSettingTest.cs
+++ launcher/CrausTest/Model/Entity/LauncherSettingTest.cs
@@ -18,6 +18,50 @@ namespace CrausTest.Model.Entity
             this.setting = new LauncherSetting();
         }
 
+        [Test]
+        public void GetStringSuccess()
+        {
+            var value = "test";
+            var key = LauncherSetting.Key.WINDOW_HEIGHT;
+            this.setting.Set( key, value );
+            Assert.AreEqual( value, this.setting.GetString( key ) );
+        }
+
+        [Test]
+        public void GetIntSuccess()
+        {
+            var value = 1;
+            var key = LauncherSetting.Key.WINDOW_HEIGHT;
+            this.setting.Set( key, value );
+            Assert.AreEqual( value, this.setting.GetInt( key ) );
+        }
+
+        [Test]
+        [ExpectedException( typeof( KeyNotFoundException ) )]
+        public void GetStringFail()
+        {
+            var key = LauncherSetting.Key.WINDOW_HEIGHT;
+            this.setting.GetString( key );
+        }
+
+        [Test]
+        [ExpectedException( typeof( KeyNotFoundException ) )]
+        public void GetIntFail()
+        {
+            var key = LauncherSetting.Key.WINDOW_HEIGHT;
+            this.setting.GetInt( key );
+        }
+
+        [Test]
+        [ExpectedException( typeof( FormatException ) )]
+        public void GetIntFormatFail()
+        {
+            var value = "abc";
+            var key = LauncherSetting.Key.WINDOW_HEIGHT;
+            this.setting.Set( key, value );
+            this.setting.GetInt( key );
+        }
+
         // TODO [実装]
 
     }
--- launcher/craus/Model/Entity/LauncherSetting.cs
+++ launcher/craus/Model/Entity/LauncherSetting.cs
@@ -5,6 +5,9 @@ using System.Text;
 
 namespace Craus.Model.Entity
 {
+    /// <summary>
+    /// 設定値を保持するクラスです。
+    /// </summary>
     public class LauncherSetting
     {
         /// <summary>
@@ -12,34 +15,113 @@ namespace Craus.Model.Entity
         /// </summary>
         public enum Key
         {
-            // TODO [実装]
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT,
         }
 
+        /// <summary>
+        /// 設定値
+        /// </summary>
+        private Dictionary<Key, String> param = null;
+
+        /// <summary>
+        /// 何も登録されていない設定を生成します。
+        /// </summary>
         public LauncherSetting()
         {
-            // TODO [実装]
+            this.param = new Dictionary<Key, string>();
         }
 
+        /// <summary>
+        /// 同じ設定値を持つインスタンスを生成します。
+        /// </summary>
+        /// <param name="original">元になる設定</param>
+        /// <exception cref="ArgumentNullException">引数がnullの場合</exception>
         public LauncherSetting( LauncherSetting original )
         {
-            // TODO [実装]
+            if( original == null )
+            {
+                throw new ArgumentNullException( "original" );
+            }
+
+            this.param = new Dictionary<Key, string>( original.param );
         }
 
+        /// <summary>
+        /// 設定値を追加します。
+        /// 追加済みの設定は上書きされます。
+        /// </summary>
+        /// <param name="key">設定名</param>
+        /// <param name="value">値</param>
+        /// <exception cref="ArgumentNullException">引数がnullの場合</exception>
         public void Set( Key key, String value )
         {
-            // TODO [実装]
+            if( value == null )
+            {
+                throw new ArgumentNullException( "value" );
+            }
+
+            if( this.param.ContainsKey( key ) )
+            {
+                this.param.Remove( key );
+            }
+
+            this.param.Add( key, value );
         }
 
+
+        /// <summary>
+        /// 設定値を追加します。
+        /// 追加済みの設定は上書きされます。
+        /// </summary>
+        /// <param name="key">設定名</param>
+        /// <param name="value">値</param>
+        /// <exception cref="ArgumentNullException">引数がnullの場合</exception>
+        public void Set( Key key, int value )
+        {
+            this.Set( key, value.ToString() );
+        }
+
+        /// <summary>
+        /// 設定値を文字列として取得します。
+        /// </summary>
+        /// <param name="key">設定名</param>
+        /// <returns>設定値</returns>
+        /// <exception cref="KeyNofFoundException">取得対象が未設定の場合</exception>
         public String GetString( Key key )
         {
-            // TODO [実装]
-            return "";
+            if( this.param.ContainsKey( key ) )
+            {
+                return this.param[key];
+            }
+            else
+            {
+                throw new KeyNotFoundException( 
+                    "[" + key + "]は未設定です。" );
+            }
         }
 
+        /// <summary>
+        /// 設定値を数値として取得します。
+        /// </summary>
+        /// <param name="key">設定名</param>
+        /// <returns>設定値</returns>
+        /// <exception cref="KeyNofFoundException">取得対象が未設定の場合</exception>
+        /// <exception cref="FormatException">設定値が数値でない場合</exception>
         public int GetInt( Key key )
         {
-            // TODO [実装]
-            return 0;
+            String value = this.GetString( key );
+
+            try
+            {
+                return int.Parse( value );
+            }
+            catch( Exception e )
+            {
+                throw new FormatException(
+                    "[" + key + "]は非数値[" + value + "]",
+                    e );
+            }
         }
     }
 }
