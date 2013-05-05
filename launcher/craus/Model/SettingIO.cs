a2611439bafd1d1829a2f1b8e4f85a3d766fccfa
Tanabe <6isstrong@gmail.com>
2013-05-05 21:36:24 +0900

    SettingIOのスケルトンを作成
    Modelパッケージ内を実装中

---
+++ launcher/craus/Model/SettingIO.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Craus.Util;
+using Craus.Model.Entity;
+
+namespace Craus.Model
+{
+    /// <summary>
+    /// 設定の保存・読み込みを行うクラスです。
+    /// </summary>
+    public class SettingIO
+    {
+        private ILogger logger = null;
+
+        public SettingIO()
+        {
+            this.logger = LogUtil.GetLogger();
+        }
+
+        /// <summary>
+        /// 設定を読み込みます。
+        /// </summary>
+        /// <returns>設定値</returns>
+        public SettingContainerForImmutable Load()
+        {
+            // TODO [実装]
+            return null;
+        }
+
+        /// <summary>
+        /// 設定を保存します。
+        /// </summary>
+        /// <param name="setting">設定値</param>
+        /// <exception cref="ArgumentNullException">引数または設定値がnullの場合</exception>
+        public void Save( SettingContainerForMutable setting )
+        {
+            if( setting == null )
+            {
+                throw new ArgumentNullException( "setting" );
+            }
+            if( setting.LauncherSetting == null )
+            {
+                throw new ArgumentNullException( "setting.LauncherSetting" );
+            }
+            if( setting.ShortcutSetting == null )
+            {
+                throw new ArgumentNullException( "setting.ShortcutSetting" );
+            }
+            if( setting.GroupSetting == null )
+            {
+                throw new ArgumentNullException( "setting.GroupSetting" );
+            }
+
+            // TODO [実装]
+        }
+    }
+
+}
