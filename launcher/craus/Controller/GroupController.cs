6ec8ba46698342d80cce1a7154fca5c1d353c9f5
Tanabe <6isstrong@gmail.com>
2013-05-11 20:49:56 +0900

    GroupControllerを新規作成
    Controllerパッケージ内を実装中

---
+++ launcher/craus/Controller/GroupController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Craus.Util;
+
+namespace Craus.Controller
+{
+    class GroupController
+    {
+        private ILogger logger = null;
+
+        /// <summary>
+        /// 初期設定
+        /// </summary>
+        public GroupController()
+        {
+            this.logger = LogUtil.GetLogger();
+        }
+
+        /// <summary>
+        /// 指定グループのショートカットを表示します。
+        /// </summary>
+        public void ViewShortcutList()
+        {
+            try
+            {
+                this.logger.Start();
+
+                // TODO [実装]
+            }
+            finally
+            {
+                this.logger.End();
+            }
+        }
+
+        /// <summary>
+        /// グループの表示を終了します。
+        /// </summary>
+        public void ExitGroupView()
+        {
+            try
+            {
+                this.logger.Start();
+
+                // TODO [実装]
+            }
+            finally
+            {
+                this.logger.End();
+            }
+        }
+    }
+}
